01842c598359839381b1d5ebcb7f9756fc038f54
sharpienotes <sharpienotes@users.noreply.github.com>
2017-03-01 13:33:53 +0100

    Added to folder.

---
+++ sandbox/building_blocks/library.py
@@ -0,0 +1,28 @@
+import itertools
+import numpy as np
+
+i = [1, 1, 1]
+j = [0, 1, 2]
+c = [i,j]
+
+# equivalent to nested for loops:
+for element in itertools.product([i],[j],repeat=2):
+    new_list = element
+    print(new_list)
+    print(new_list[1])
+    aaa = new_list[1]
+    print(aaa[1])
+
+# arrays and stuff:
+sphere_here = np.empty([3,3,3], dtype = complex)
+a = np.arange(15).reshape(3, 5)
+print(a)
+print(a.ndim, a.shape)
+print(a.itemsize)
+print(a.size)
+
+b = (1,2,3,4)
+print(len(b))
+
+
+
