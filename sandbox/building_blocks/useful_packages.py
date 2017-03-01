2e3e81ff9cf230754ec604384ac4a25cf0d59a3e
sharpienotes <sharpienotes@users.noreply.github.com>
2017-03-01 11:38:23 +0100

    Added to folder.

---
+++ sandbox/building_blocks/useful_packages.py
@@ -0,0 +1,11 @@
+import seaborn
+import matplotlib
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import nibabel as nb
+import numpy as np
+
+# seaborn and matplotlib for plotting
+# nilearn for machine learning on brainy stuff
