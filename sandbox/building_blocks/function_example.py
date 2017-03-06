d72f5559dc96db6f98a7f10bc1fe52854b37760f
sharpienotes <sharpienotes@users.noreply.github.com>
2017-03-06 13:48:54 +0100

    see library

--- sandbox/building_blocks/function_example.py
+++ sandbox/building_blocks/function_example.py
@@ -15,3 +15,32 @@ def summing(
         17
     """
     return a + b
+
+# defining a function:
+# if want to be able to call empty, function() then specifying as follows is a good idea:
+def function(a=None, b=None, point=None):
+    """
+
+    Args:
+        a ():
+        b ():
+
+    Returns: the sum of a and b
+
+    """
+    if not a:
+        a=5
+    if not b:
+        b=6
+
+    # if want to leave this empty but the parameter is an array:
+    # use:
+    if point is None:
+        point = (1,2,3)
+
+    other_point = point + a
+    print(other_point)
+
+    return a+b
+
+function() # calls the function without specified non-default parameters
